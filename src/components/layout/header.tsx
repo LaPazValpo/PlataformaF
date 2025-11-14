de04b8a86656c7d5cb7002334d5e574a8fd6ec9f
Patricio <lapazdecristovalpo@gmail.com>
2025-11-14 21:54:16 +0000

    quita el boton test auth

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -88,14 +88,6 @@ const Header = () => {
                     <Lock className="mr-2 h-4 w-4" />
                     Intranet
                   </Link>
-                  <Link
-                    href="/test-auth"
-                    onClick={() => setIsMobileMenuOpen(false)}
-                    className='text-lg text-muted-foreground hover:text-primary flex items-center'
-                  >
-                    <ShieldAlert className="mr-2 h-4 w-4" />
-                    Test Auth
-                  </Link>
               </nav>
             </SheetContent>
           </Sheet>
@@ -175,11 +167,6 @@ const Header = () => {
             <Button variant="outline" size="sm" asChild>
               <Link href="/login">
                 <Lock className="mr-2 h-4 w-4" /> Intranet
-              </Link>
-            </Button>
-             <Button variant="destructive" size="sm" asChild>
-              <Link href="/test-auth">
-                <ShieldAlert className="mr-2 h-4 w-4" /> Test Auth
               </Link>
             </Button>
         </nav>
