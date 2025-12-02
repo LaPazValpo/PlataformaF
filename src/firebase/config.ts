8c20e8c55667f69efa503f3ab7560adab4844bf1
Patricio <lapazdecristovalpo@gmail.com>
2025-12-02 13:36:33 +0000

    sigue el problema

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,3 +1,3 @@
 // It's safe to expose this configuration to the client
 // See https://firebase.google.com/docs/projects/learn-more#config-files-objects
-export const firebaseConfig = {"projectId":"studio-1401488026-1e65b","appId":"1:1036158902994:web:62ac144fe461ac4d2c030b","apiKey":"AIzaSyCKffL9mta2Ztu2WxtqBPnLV5wCBQ2ZE58","authDomain":"studio-1401488026-1e65b.firebaseapp.com","measurementId":"","messagingSenderId":"1036158902994"};
+export const firebaseConfig = {"projectId":"studio-1401488026-1e65b","appId":"1:1036158902994:web:62ac144fe461ac4d2c030b","apiKey":"REEMPLAZA-ESTO-CON-TU-NUEVA-CLAVE-DE-API","authDomain":"studio-1401488026-1e65b.firebaseapp.com","measurementId":"","messagingSenderId":"1036158902994"};
